970cf4f670f063f67e92abd7b6b0edaa1fcb501d
Naksila <85428747+Naksila@users.noreply.github.com>
2021-06-16 23:48:43 +0700

    Update Prevent .dart

--- lib/widgets/Prevent .dart
+++ lib/widgets/Prevent .dart
@@ -1,5 +1,6 @@
 import 'package:covid_app/Model/PreventModel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Prevent extends StatefulWidget {
   @override
@@ -24,65 +25,56 @@ class ItemList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.centerLeft,
-      children: [
-        Container(
-          height: 150,
-          width: 370,
-          margin: EdgeInsets.all(15),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
+    return Container(
+        height: 160,
+        width: 360,
+        margin: EdgeInsets.only(bottom: 15),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+          child: Card(
+            elevation: 15,
             color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Color(0xffd4d4e4),
-                blurRadius: 2,
-                spreadRadius: 0,
-                offset: Offset(3, 3),
-              )
-            ],
-          ),
-        ),
-        Image(
-          image: AssetImage(preventModel.image),
-          width: 155,
-        ),
-        Positioned(
-          left: 130,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            height: 136,
-            width: MediaQuery.of(context).size.width - 175,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  preventModel.title,
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
-                  ),
+            shadowColor: Colors.grey,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image(
+                  image: AssetImage(preventModel.image),
+                  width: 140,
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Expanded(
-                  child: Text(
-                    preventModel.text,
-                    maxLines: 4,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
+                Container(
+                  height: 150,
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  width: MediaQuery.of(context).size.width - 188,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        preventModel.title,
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        preventModel.text,
+                        maxLines: 4,
+                        overflow: TextOverflow.clip,
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-        ),
-      ],
-    );
+        ));
   }
 }
+
